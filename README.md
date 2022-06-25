4e0fd0fa34b822d7ef9c5f24bea7d5607e987e73
fujifruity <fujifruity@gmail.com>
2022-06-25 20:05:46 +0900

    fix: dark bg color for dark page

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # userscripts
-Greasemonkey userscripts in sync with Greasy Fork (https://greasyfork.org/en/users/698254-fujifruity)
+Greasemonkey userscripts in sync with Greasy Fork (https://greasyfork.org/en/users/698254-fujifruity).
+
+## NoMouseGoogle
+Shortcut for Google search results. j/k to move focus, l/h to open in new/background tab.
+
+<img src="https://raw.githubusercontent.com/fujifruity/userscripts/master/res/NoMouseGoogle.gif">
+
+## TableOfContents
+Create a table of contents for the page. ctrl+alt+t to show; click an item to jump; escape or click outside to close.
+
+<img src="https://raw.githubusercontent.com/fujifruity/userscripts/master/res/TableOfContents.gif">
--- TableOfContents.user.js
+++ TableOfContents.user.js
@@ -1,22 +1,17 @@
 // ==UserScript==
 // @name         TableOfContents
 // @namespace    com.gmail.fujifruity.greasemonkey
-// @version      1.1
-// @description  Create a table of contents from h-tags. ctrl+alt+t to show; escape or click outside to close.
+// @version      1.2
+// @description  Create a table of contents for the page. ctrl+alt+t to show; click an item to jump; escape or click outside to close.
 // @author       fujifruity
 // @match        *://*/*
 // @grant        none
 // ==/UserScript==
 
 {
-    const log = (...msg) => console.log('TableOfContents', ...msg)
-
     const modalId = 'fujifruity-toc'
-
     const getModal = () => document.getElementById(modalId) ?? createModal()
-
     const createModal = () => {
-        log('create toc')
         const modal = document.createElement('div')
         modal.id = modalId
         modal.style.zIndex = 99999
@@ -26,34 +21,32 @@
         modal.style.padding = '1em'
         modal.style.margin = '1em'
         modal.style.overflowY = 'auto'
-        modal.style.backgroundColor = 'white'
+        const color = window.getComputedStyle(document.body, null).backgroundColor
+        const isRgba = color.match(/,/g).length == 3
+        const rgb = isRgba ? color.replace(/, \d+\)/, ')') : color
+        modal.style.backgroundColor = rgb
         modal.style.cursor = 'pointer'
         modal.style.boxShadow = '0 2px 10px rgba(0,0,0,0.16)'
         document.body.prepend(modal)
         // Add headdings to the modal
         const headdings = Array.from(document.querySelectorAll("h1, h2, h3, h4, h5, h6"))
         const clonedHeaddings = headdings.map(e => e.cloneNode(true))
-        const clickableHeaddings = clonedHeaddings.map( (e, i) => {
-            e.addEventListener('click', event => headdings[i].scrollIntoView() )
+        const clickableHeaddings = clonedHeaddings.map((e, i) => {
+            e.addEventListener('click', event => headdings[i].scrollIntoView({ behavior: "smooth" }))
             return e
         })
         clickableHeaddings.forEach(e => modal.appendChild(e))
         return modal
     }
-
     const closeModal = () => {
-        log('close toc')
         const modal = getModal()
         modal.style.display = 'none'
     }
-
-    log('set shortcuts')
     window.addEventListener('keydown', event => {
         if (!event.ctrlKey || !event.altKey || event.key != 't') return
-        log('show toc')
         const modal = getModal()
         modal.style.display = 'block'
-        // set shortcut closes modal
+        // Set shortcut to close modal
         const onKeydown = event => {
             if (event.key != 'Escape') return
             closeModal()
