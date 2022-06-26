b10ba1631f8e3a75ea7bb4a4dfd3997f8e2dd1dc
fujifruity <fujifruity@gmail.com>
2022-06-26 17:50:51 +0900

    Set original bg color for each heading

--- TableOfContents.user.js
+++ TableOfContents.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         TableOfContents
 // @namespace    com.gmail.fujifruity.greasemonkey
-// @version      1.2
+// @version      1.3
 // @description  Create a table of contents for the page. ctrl+alt+t to show; click an item to jump; escape or click outside to close.
 // @author       fujifruity
 // @match        *://*/*
@@ -10,7 +10,10 @@
 
 {
     const modalId = 'fujifruity-toc'
-    const getModal = () => document.getElementById(modalId) ?? createModal()
+    const getBgColor = e => {
+        const color = getComputedStyle(e).backgroundColor
+        return color.includes('rgb(') ? color : e.parentElement ? getBgColor(e.parentElement) : 'white'
+    }
     const createModal = () => {
         const modal = document.createElement('div')
         modal.id = modalId
@@ -21,23 +24,22 @@
         modal.style.padding = '1em'
         modal.style.margin = '1em'
         modal.style.overflowY = 'auto'
-        const color = window.getComputedStyle(document.body, null).backgroundColor
-        const isRgba = color.match(/,/g).length == 3
-        const rgb = isRgba ? color.replace(/, \d+\)/, ')') : color
-        modal.style.backgroundColor = rgb
         modal.style.cursor = 'pointer'
         modal.style.boxShadow = '0 2px 10px rgba(0,0,0,0.16)'
+        modal.style.backgroundColor = getBgColor(document.body)
         document.body.prepend(modal)
         // Add headdings to the modal
         const headdings = Array.from(document.querySelectorAll("h1, h2, h3, h4, h5, h6"))
         const clonedHeaddings = headdings.map(e => e.cloneNode(true))
         const clickableHeaddings = clonedHeaddings.map((e, i) => {
+            e.style.backgroundColor = getBgColor(headdings[i])
             e.addEventListener('click', event => headdings[i].scrollIntoView({ behavior: "smooth" }))
             return e
         })
         clickableHeaddings.forEach(e => modal.appendChild(e))
         return modal
     }
+    const getModal = () => document.getElementById(modalId) ?? createModal()
     const closeModal = () => {
         const modal = getModal()
         modal.style.display = 'none'
