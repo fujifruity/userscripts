73dcd8939823161146fb21dd1449c6bbdb03915e
fuji <fdc.fuji@gmail.com>
2023-09-16 15:08:44 +0800

    scroll only if next item is out of screen

--- NoMouseGoogle.user.js
+++ NoMouseGoogle.user.js
@@ -1,14 +1,13 @@
 // ==UserScript==
 // @name         NoMouseGoogle
 // @namespace    com.gmail.fujifruity.greasemonkey
-// @version      1.16
+// @version      1.17
 // @description  Shortcut for Google search results. j/k to move focus, enter/l/h to open in current/new/background tab, n/p to go to next/previous page.
 // @author       fujifruity
 // @include      https://www.google.com/search*
 // @include      https://www.google.co.*/search*
 // @grant        GM.openInTab
 // @license      MIT
-// @downloadURL none
 // ==/UserScript==
 
 {
@@ -16,15 +15,12 @@
     const itemQuery = "#res div[data-hveid][data-ved][lang], #botstuff div[data-hveid][data-ved][lang], #rso video-voyager>div"
     const findItems = () => [...document.querySelectorAll(itemQuery)]
         .filter(e => e.offsetParent != null /* is visible */)
-    const findCurrentItem = items => items.find(e => e.hasAttribute(tag))
-    const moveCursor = step => {
+    const findCurrentItem = (items) => items.find(e => e.hasAttribute(tag))
+    const moveCursor = (step) => {
         const items = findItems()
         const currentItem = findCurrentItem(items)
-        const r = currentItem.getBoundingClientRect();
-        const inScreen = (0 < r.top && r.top < window.innerHeight
-            || 0 < r.bottom && r.bottom < window.innerHeight)
-        if (!inScreen) {
-            const dist = e => {
+        if (!isVisible(currentItem, false)) {
+            const dist = (e) => {
                 const r = e.getBoundingClientRect()
                 return Math.abs(window.innerHeight - (r.top + r.bottom))
             }
@@ -35,8 +31,14 @@
         const nextIdx = (items.indexOf(currentItem) + step + items.length) % items.length
         select(items[nextIdx], currentItem)
     }
-    const highlight = e => {
-        const isDarkTheme = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches
+    const isVisible = (item, fullyVisible) => {
+        const rect = item.getBoundingClientRect();
+        const isTopVisible = 0 < rect.top && rect.top < window.innerHeight
+        const isBottomVisible = 0 < rect.bottom && rect.bottom < window.innerHeight
+        return fullyVisible ? isTopVisible && isBottomVisible : isTopVisible || isBottomVisible
+    }
+    const highlight = (e) => {
+        const isDarkTheme = window.matchMedia?.('(prefers-color-scheme: dark)')?.matches
         e.style.backgroundColor = isDarkTheme ? '#2a2a2a' : 'WhiteSmoke'
     }
     const select = (item, currentItem) => {
@@ -48,12 +50,15 @@
         // Select the item.
         item.setAttribute(tag, '')
         highlight(item)
-        item.scrollIntoView({ behavior: "smooth", block: "center" })
+        // Scroll only if the item is not fully visible
+        if (!isVisible(item, true)) {
+            item.scrollIntoView({ behavior: "smooth", block: "center" })
+        }
         console.log('select', item)
     }
 
     const currentItemHref = () => findCurrentItem(findItems()).querySelector('a').href
-    const openInNewTab = inBackground => GM.openInTab(currentItemHref(), inBackground)
+    const openInNewTab = (inBackground) => GM.openInTab(currentItemHref(), inBackground)
     const openInThisTab = () => window.open(currentItemHref(), "_self")
 
     // Select the first item without scrolling.
